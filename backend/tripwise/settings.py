e5fa8de850c1a576593bbbd4ce923885e4dda859
Tendo Calvin <tendo.calvin18@gmail.com>
2026-09-02 10:12:46 +0300

    docs:updating the database credentials for Tripwise

--- backend/tripwise/settings.py
+++ backend/tripwise/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'tripwise.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
+        "NAME": os.getenv("PGDATABASE"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "HOST": os.getenv("PGHOST"),
+        "PORT": os.getenv("PGPORT"),
     }
 }
 
