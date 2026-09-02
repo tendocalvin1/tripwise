23ad8c7bdbd00154b89959d05404d41a22781fd5
Tendo Calvin <tendo.calvin18@gmail.com>
2026-09-02 09:18:44 +0300

    feat:defining the end points of the project

--- backend/tripwise/urls.py
+++ backend/tripwise/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# This page is for end points for the project
 
 urlpatterns = [
     path('admin/', admin.site.urls),
