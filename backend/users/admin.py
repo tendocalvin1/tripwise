8ff37f11d147706e6b9d905bc53502a0946cbd1c
Tendo Calvin <tendo.calvin18@gmail.com>
2026-09-04 10:36:13 +0300

    feat: register UserManager, User, and UserPreference models in admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserManager, User, UserPreference
 
 # Register your models here.
+admin.site.register([
+    'UserManager',
+    'User',
+    'UserPreference'
+])
